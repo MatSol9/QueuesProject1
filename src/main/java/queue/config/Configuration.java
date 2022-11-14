57ba25d6857f1254ac5a7e4e08a6cc5a682e0e52
MatSol9 <mateusz.soltys@protonmail.com>
2022-11-14 19:31:55 +0100

    Added connection to proxy

--- src/main/java/queue/config/Configuration.java
+++ src/main/java/queue/config/Configuration.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import static org.mockito.Mockito.mock;
-
 @org.springframework.context.annotation.Configuration
 @EnableScheduling
 public class Configuration {
 
     @Bean
     WebClient webClient() {
-        return mock(WebClient.class);
+        return WebClient.create();
     }
 }
--- src/main/java/queue/services/TrafficService.java
+++ src/main/java/queue/services/TrafficService.java
@@ -1,20 +1,35 @@
 package queue.services;
 
+import model.BasicEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import requests.BasicDataRequest;
+
+import java.util.Objects;
 
 @Service
 public class TrafficService {
     private static final Logger LOGGER = LoggerFactory.getLogger(TrafficService.class);
     private final WebClient webClient;
+    @Value("${connections.proxy}")
+    private String proxyUrl;
 
     public TrafficService(WebClient webClient) {
         this.webClient = webClient;
     }
 
     public void sendRequest() {
-        LOGGER.info("Sending request");
+        LOGGER.info("Sending request to {}", proxyUrl);
+        BasicEntry basicEntry = webClient.method(HttpMethod.GET)
+                .uri(proxyUrl + "/data/")
+                .bodyValue(new BasicDataRequest("Loc"))
+                .retrieve()
+                .bodyToMono(BasicEntry.class)
+                .block();
+        LOGGER.info("Received {}", basicEntry);
     }
 }
