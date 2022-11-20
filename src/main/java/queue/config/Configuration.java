816bd07379643734b45d40bd3feac773ac492e49
MatSol9 <mateusz.soltys@protonmail.com>
2022-11-20 22:26:43 +0100

    Fixed multithreaded queue

--- src/main/java/queue/config/Configuration.java
+++ src/main/java/queue/config/Configuration.java
@@ -2,7 +2,13 @@ package queue.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @org.springframework.context.annotation.Configuration
 @EnableScheduling
@@ -11,5 +17,15 @@ public class Configuration {
     @Bean
     WebClient webClient() {
         return WebClient.create();
+    }
+
+    @Bean
+    public Executor scheduledTaskThreadPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1000);
+        executor.setMaxPoolSize(1000);
+        executor.setThreadNamePrefix("name-");
+        executor.initialize();
+        return executor;
     }
 }
--- src/main/java/queue/traffic/TrafficGenerator.java
+++ src/main/java/queue/traffic/TrafficGenerator.java
@@ -1,23 +1,39 @@
 package queue.traffic;
 
+import model.BasicEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import queue.services.TrafficService;
+import requests.BasicDataRequest;
 
 @Service
 public class TrafficGenerator {
     private static final Logger LOGGER = LoggerFactory.getLogger(TrafficGenerator.class);
     private final TrafficService trafficService;
+    private final WebClient webClient;
+    @Value("${connections.proxy}")
+    private String proxyUrl;
 
-    public TrafficGenerator(TrafficService trafficService) {
+    public TrafficGenerator(TrafficService trafficService, WebClient webClient) {
         this.trafficService = trafficService;
+        this.webClient = webClient;
     }
 
     @Scheduled(fixedRate = 1000)
     void sendRequest() {
         LOGGER.info("Creating request");
-        trafficService.sendRequest();
+        LOGGER.info("Sending request to {}", proxyUrl);
+        webClient.method(HttpMethod.GET)
+                .uri(proxyUrl + "/data/")
+                .bodyValue(new BasicDataRequest("Loc"))
+                .retrieve()
+                        .bodyToMono(BasicEntry.class)
+                .elapsed()
+                                .subscribe(basicEntry -> LOGGER.info("Received {}", basicEntry.getT1()));
     }
 }
